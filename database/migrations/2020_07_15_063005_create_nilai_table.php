25b322c894e90a8971a72a5b014e78acb5f70637
antoniofendy <layfenfen@gmail.com>
2020-07-15 17:16:06 +0700

    Adding Projet

---
+++ database/migrations/2020_07_15_063005_create_nilai_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateNilaiTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('nilai', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('matkul_id');
+            $table->unsignedBigInteger('mhs_id');
+            $table->timestamps();
+
+            $table->foreign('matkul_id')->references('id')->on('mata_kuliah')
+            ->onDelete('cascade')
+            ->onUpdate('cascade');
+
+            $table->foreign('mhs_id')->references('id')->on('mahasiswa')
+            ->onDelete('cascade')
+            ->onUpdate('cascade');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('nilai');
+    }
+}
